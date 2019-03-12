61fbf24f0852b560c08d8736f666493a1e63a730
Øivind Albrigtsen <formalatist@gmail.com>
2019-03-12 15:44:54 +0100

    Renamed type

--- src/Perlin.cs
+++ src/Perlin.cs
@@ -1,17 +1,17 @@
 using System;
 
-public abstract class Perlin<GradientStorageType> {
+public abstract class Perlin<GradientType> {
 
 	//The function we use to smooth the interpolation between the
 	//different corners of the cube. With a linear interpolation
 	//we'll get hard edges.
 	private Func<double, double> SmoothingFunction;
-	private GradientStorageType[] gradients;
+	private GradientType[] gradients;
 	//Function that performs the dot product (inner product) of two 3D vectors
-	//where one of the vectors is stored in the GradientStorageType type.
-	private Func<GradientStorageType, double, double, double, double> Dot;
+	//where one of the vectors is stored in the GradientType type.
+	private Func<GradientType, double, double, double, double> Dot;
 
-	protected Perlin(GradientStorageType[] gradients, Func<GradientStorageType, double, double, double, double> dot, Func<double, double> smoothingFunction) {
+	protected Perlin(GradientType[] gradients, Func<GradientType, double, double, double, double> dot, Func<double, double> smoothingFunction) {
 		this.gradients = gradients;
 		this.Dot = dot;
 		this.SmoothingFunction = smoothingFunction;
@@ -37,6 +37,7 @@ public abstract class Perlin<GradientStorageType> {
 	}
 }
 
+//Performant Perlin class that stores gradients as ints
 public class Perlin : Perlin<int> {
 
 	private static int[] gradients = {5};
