7dc959e5a83430965102bba6cf727a3326ad9220
albrigtsen <oivinal@stud.ntnu.no>
2019-03-12 04:08:32 +0100

    Made Perlin generic
    This is to allow for different encoding schemes for gradients. Some other ways might be more performant(?). Or they might interface better with already existing code.

--- src/Perlin.cs
+++ src/Perlin.cs
@@ -1,7 +1,53 @@
-public class Perlin {
+using System;
 
-	public Perlin() {
-		
+public abstract class Perlin<GradientStorageType> {
+
+	//The function we use to smooth the interpolation between the
+	//different corners of the cube. With a linear interpolation
+	//we'll get hard edges.
+	private Func<double, double> SmoothingFunction;
+	private GradientStorageType[] gradients;
+	//Function that performs the dot product (inner product) of two 3D vectors
+	//where one of the vectors is stored in the GradientStorageType type.
+	private Func<GradientStorageType, double, double, double, double> Dot;
+
+	protected Perlin(GradientStorageType[] gradients, Func<GradientStorageType, double, double, double, double> dot, Func<double, double> smoothingFunction) {
+		this.gradients = gradients;
+		this.Dot = dot;
+		this.SmoothingFunction = smoothingFunction;
 	}
 
+	public double Noise(double x, double y, double z = 0.5d) {
+
+
+
+		return 0;
+	}
+
+
+	private static double LinearlyInterpolate(double valueA, double valueB, double t) {
+		return valueA + t*(valueB - valueA);
+	}
+
+	//Takes a val in the range 0..1 and returns an s-curve
+	//in the range 0..1
+	//from Perlins original paper TODO: add link to paper
+	protected static double SmoothToSCurve(double val) {
+		return val*val*val*(val*(val*6d-15d)+10d);
+	}
+}
+
+public class Perlin : Perlin<int> {
+
+	private static int[] gradients = {5};
+
+	public Perlin(Func<double, double> smoothingFunction) : base(gradients, Dot, smoothingFunction) {
+		Console.WriteLine("Perlin<int>");
+	}
+
+	public Perlin() : this(SmoothToSCurve){}
+
+	private static double Dot(int gradient, double x, double y, double z) {
+		return 0d;
+	}
 }
