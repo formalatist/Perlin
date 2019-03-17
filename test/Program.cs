9a48a4be19c9147de761a77107b54ae900ece1c2
albrigtsen <oivinal@stud.ntnu.no>
2019-03-17 07:43:55 +0100

    Code for creating a smoothly varying 2D noise image.

--- test/Program.cs
+++ test/Program.cs
@@ -8,26 +8,29 @@ public class Program {
 
 		var perlin = new Perlin();
 
-		int size = 512;
+		int size = 1024;
 		Bitmap bitmap = new Bitmap(size, size);
 
 		// System.Diagnostics.Debugger.Launch();
 
-		for(int x = 0; x < size; x++) {
-			for(int y = 0; y < size; y++) {
-				int color = (int)(128*(1d+perlin.Noise((double)x/64, (double)y/64)));
-				if(color < 0) {
-					Console.WriteLine(color);
-					color = 0;
-				} else if(color > 255) {
-					Console.WriteLine(color);
-					color = 255;
-				}
-				bitmap.SetPixel(x, y, Color.FromArgb(color, color, color));
-			 }
+		for(int z = 0; z < 480; z++) {
+
+			for(int x = 0; x < size; x++) {
+				for(int y = 0; y < size; y++) {
+					int color = (int)(127*(1d+perlin.Noise((double)x/128, (double)y/128, (double)z/120)));
+					// if(color < 0) {
+					// 	Console.WriteLine(color);
+					// 	color = 0;
+					// } else if(color > 255) {
+					// 	Console.WriteLine(color);
+					// 	color = 255;
+					// }
+					bitmap.SetPixel(x, y, Color.FromArgb(color, color, color));
+				 }
+			}
+			bitmap.Save("test" + z + ".png");
 		}
 
-		bitmap.Save("test.png");
 		Console.ReadLine();
 	}
 
